0540a48c89fae0dad2b9a1dbadd1dadbc2c2a008
n-i-x <440158+n-i-x@users.noreply.github.com>
2025-10-30 16:15:17 -0400

    Update ledspicer to 0.6.4

--- package/batocera/utils/ledspicer/ledspicer.mk
+++ package/batocera/utils/ledspicer/ledspicer.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LEDSPICER_VERSION = 0.6.3.1
+LEDSPICER_VERSION = 0.6.4
 LEDSPICER_SITE = $(call github,meduzapat,LEDSpicer,$(LEDSPICER_VERSION))
 LEDSPICER_LICENSE = GPLv3
 LEDSPICER_DEPENDENCIES = tinyxml2 libusb libtool udev libpthread-stubs
